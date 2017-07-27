079ae5f93f6209f22dfdd9a1362da0516183b0e4
crater0516 <crater0516@naver.com>
2017-07-27 17:04:09 +0900

    random

---
+++ random/getkey.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main () {
+	unsigned int random = rand();
+	unsigned int key = 0;
+	key = 0xdeadbeef ^ random;
+
+	printf("%u\n", key);
+	return 0;
+}
